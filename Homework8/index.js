a83a3930d973240663b4d860c2747c2e75bc6f5e
Qyea <kreshess@mail.ru>
2024-06-18 12:32:28 +0300

    practice 10

---
+++ Homework8/index.js
@@ -0,0 +1,12 @@
+const getWeather = () => {
+  const userInputElement = document.getElementById('user-input');
+  console.log(userInputElement.value);
+  /* Algorithm
+1. Get the input element from the DOM
+2. Take the user input value
+3. Perfrom validation (check wether user input value is not empty)
+4. Get the data from Weather API
+5. Display the data.
+6. Handle errors
+*/
+};
